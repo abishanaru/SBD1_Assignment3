e0d751442f84836e3aaf63e469caa45ed741a7c6
abishanaru <aruma2@bfh.ch>
2023-06-09 00:58:29 +0200

    Heatmap und Top 10 Tabelle hinzugefügt

--- Assignment3/app.R
+++ Assignment3/app.R
@@ -2,7 +2,10 @@ library(shiny)
 library(leaflet)
 library(shinydashboard)
 library(shinyWidgets)
-
+library(rvest)
+library(readr)
+library(leaflet.extras)
+library(DT)
 
 # Funktion zum Scrapen der Daten von der Website
 scrape_covid_data <- function() {
@@ -12,9 +15,9 @@ scrape_covid_data <- function() {
   # Scrape the table containing coronavirus data
   table <- html_table(html_nodes(page, "table#main_table_countries_today"), header = TRUE)[[1]]
   
-  # Die ersten 9 und die letzten 8 Zeilen beinhalten Angaben zu den Kontinenten,
+  # Die ersten 8 und die letzten 8 Zeilen beinhalten Angaben zu den Kontinenten,
   # die für die Kartenvisualisierung nicht benötigt werden.
-  # Lösche die ersten 9 Zeilen
+  # Lösche die ersten 8 Zeilen
   table <- table[-c(1:8), ]
   
   # Lösche die letzten 8 Zeilen
@@ -62,7 +65,8 @@ ui <- dashboardPage(
   dashboardHeader(title = "COVID-19 Dashboard"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Karte", tabName = "map_tab")
+      menuItem("Karte", tabName = "map_tab"),
+      menuItem("Heatmap", tabName = "heatmap_tab")
     )
   ),
   dashboardBody(
@@ -101,8 +105,52 @@ ui <- dashboardPage(
         fluidRow(
           box(
             title = "Statistik",
-            width = 12,
+            width = 6,
             valueBoxOutput("selected_countries_count")
+          ),
+          box(
+            title = "Mittelwert",
+            width = 6,
+            valueBoxOutput("average_value")
+          ),
+          box(
+            title = "Maximaler Wert",
+            width = 6,
+            valueBoxOutput("max_value")
+          ),
+          box(
+            title = "Minimaler Wert",
+            width = 6,
+            valueBoxOutput("min_value")
+          )
+        )
+      ),
+      # Heatmap Tab
+      tabItem(
+        tabName = "heatmap_tab",
+        fluidRow(
+          box(
+            title = "Ausgewählter Wert",
+            width = 6,
+            selectInput(
+              inputId = "selected_column_heatmap",
+              label = "Spalte auswählen",
+              choices = c("TotalCases", "TotalDeaths", "TotalTests", "Population")
+            )
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Heatmap",
+            width = 12,
+            leafletOutput("heatmap")
+          )
+        ),
+        fluidRow(
+          box(
+            title = "Top 10 Werte",
+            width = 12,
+            dataTableOutput("top_values_table")
           )
         )
       )
@@ -175,8 +223,106 @@ server <- function(input, output, session) {
       color = "yellow"
     )
   })
+  
+  # Mittelwert der Werte der ausgewählten Länder anzeigen (Statistik Tab)
+  output$average_value <- renderValueBox({
+    merged_data <- data()
+    selected_data <- merged_data[merged_data$country %in% input$selected_countries, ]
+    
+    valueBox(
+      value = round(mean(selected_data[[input$selected_column]], na.rm = TRUE), 2),
+      subtitle = "Mittelwert",
+      icon = icon("calculator"),
+      color = "green"
+    )
+  })
+  
+  # Maximaler Wert und Name des Landes anzeigen (Statistik Tab)
+  output$max_value <- renderValueBox({
+    merged_data <- data()
+    selected_data <- merged_data[merged_data$country %in% input$selected_countries, ]
+    
+    max_value <- max(selected_data[[input$selected_column]], na.rm = TRUE)
+    country_with_max_value <- selected_data$country[selected_data[[input$selected_column]] == max_value]
+    
+    valueBox(
+      value = max_value,
+      subtitle = paste("Maximaler Wert (", country_with_max_value, ")"),
+      icon = icon("arrow-up"),
+      color = "red"
+    )
+  })
+  
+  # Minimaler Wert und Name des Landes anzeigen (Statistik Tab)
+  output$min_value <- renderValueBox({
+    merged_data <- data()
+    selected_data <- merged_data[merged_data$country %in% input$selected_countries, ]
+    
+
+    min_value <- min(selected_data[[input$selected_column]], na.rm = TRUE)
+    
+    country_with_min_value <- selected_data$country[selected_data[[input$selected_column]] == min_value]
+    
+    valueBox(
+      value = min_value,
+      subtitle = paste("Minimaler Wert (", country_with_min_value, ")"),
+      icon = icon("arrow-down"),
+      color = "blue"
+    )
+  })
+  
+  # Kreise mit variabler Farbe und Größe erstellen (Heatmap Tab)
+  output$heatmap <- renderLeaflet({
+    merged_data <- data()
+    
+    # Filtern der Daten basierend auf der ausgewählten Spalte
+    selected_data <- merged_data[merged_data$country %in% input$selected_countries, ]
+    
+    # Normalisieren der Werte für Farbe und Größe
+    values <- selected_data[[input$selected_column]]
+    values <- na.omit(values)  # Entfernen der fehlenden Werte
+    normalized_values <- scales::rescale(values, to = c(0, 1))
+    
+    # Manuelle Festlegung des Wertebereichs für die Farbpalette
+    min_value <- min(values, na.rm = TRUE)
+    max_value <- max(values, na.rm = TRUE)
+    
+    # Farbpalette definieren
+    color_palette <- colorNumeric(
+      palette = c("orange", "red"),  # Farbpalette anpassen
+      domain = c(min_value, max_value)
+    )
+    
+    # Leaflet-Karte erstellen
+    leaflet(data = selected_data) %>%
+      addTiles() %>%
+      addCircleMarkers(
+        lat = ~latitude,
+        lng = ~longitude,
+        radius = ~sqrt(normalized_values) * 10,  # Größe der Kreise anpassen
+        fillColor = ~color_palette(values),  # Farbe der Kreise anpassen
+        color = "red",
+        fillOpacity = 0.8,
+        stroke = FALSE,
+        label = ~paste0(input$selected_column, ": ", get(input$selected_column))
+      )
+  })
+
+  
+  # Tabelle mit den Top 10 Werten erstellen (Heatmap Tab)
+  output$top_values_table <- DT::renderDataTable({
+    merged_data <- data()
+    
+    # Sortieren der Daten nach der ausgewählten Spalte in absteigender Reihenfolge
+    sorted_data <- merged_data[order(merged_data[[input$selected_column_heatmap]], decreasing = TRUE), ]
+    
+    # Begrenzen auf die Top 10 Werte
+    top_values <- head(sorted_data, 10)
+    
+    # DataTable-Objekt erstellen
+    datatable(top_values, options = list(pageLength = 10))
+  })
 }
 
 # App starten
 shinyApp(ui, server)
-
